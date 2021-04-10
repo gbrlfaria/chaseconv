d6f5a0813ab6e5428e4f2843e57d9e4c4b8a246b
Gabriel Faria <gabfaria.cs@gmail.com>
2021-04-10 15:03:54 -0300

    fix

--- src/convert.rs
+++ src/convert.rs
@@ -1,10 +1,11 @@
 use crate::{asset::Asset, scene::Scene};
 
 pub trait Importer {
-    fn import(&self, asset: &Asset, scene: &mut Scene) -> Option<()>;
+    fn import(&self, asset: &Asset, scene: &mut Scene) -> Result<(), ()>;
     fn extensions(&self) -> &[&str];
 }
 
 pub trait Exporter {
-    fn export(&self, scene: &Scene) -> Option<Vec<Asset>>;
+    fn export(&self, scene: &Scene) -> Result<Vec<Asset>, ()>;
+    fn extensions(&self) -> &[&str];
 }
