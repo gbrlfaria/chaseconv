cea4bb0517e7319148967b656ca8335d60f17399
Gabriel Faria <gabfaria.cs@gmail.com>
2021-12-12 07:29:19 -0300

    adjust

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,33 @@ use chaseconv::conversion;
 fn main() {
     let files: Vec<_> = std::env::args().skip(1).collect();
 
-    eprintln!("Trying to convert {} file(s)...\n", files.len());
+    if !files.is_empty() {
+        eprintln!("Trying to convert {} file(s)...\n", files.len());
 
-    let converters = conversion::converters();
+        let converters = conversion::converters();
 
-    let items: Vec<_> = converters.iter().map(|converter| converter.name).collect();
-    let option = dialoguer::Select::new()
-        .with_prompt("Select the format you want to convert the input files to")
-        .default(0)
-        .items(&items)
-        .interact()
-        .expect("Failed to select converter option");
-    let converter = &converters[option];
+        let items: Vec<_> = converters.iter().map(|converter| converter.name).collect();
+        let option = dialoguer::Select::new()
+            .with_prompt("Select the format you want to convert the input files to")
+            .default(0)
+            .items(&items)
+            .interact()
+            .expect("Failed to select converter option");
+        let converter = &converters[option];
 
-    let out_path = dialoguer::Input::new()
-        .with_prompt("Select the output directory")
-        .default(String::from("output/"))
-        .show_default(true)
-        .interact()
-        .expect("Failed to define output path");
+        let out_path = dialoguer::Input::new()
+            .with_prompt("Select the output directory")
+            .default(String::from("output/"))
+            .show_default(true)
+            .interact()
+            .expect("Failed to define output path");
 
-    eprintln!();
-    converter.convert(&files, &out_path);
+        eprintln!();
+        converter.convert(&files, &out_path);
+    } else {
+        eprintln!("There were no input files. No files were converted.")
+    }
 
-    eprintln!();
     pause();
 }
 
@@ -38,7 +41,8 @@ fn pause() {
     let mut stdin = io::stdin();
     let mut stdout = io::stdout();
 
-    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    // We want the cursor to stay at the end of the line,
+    // so we print without a newline and flush manually.
     write!(stdout, "Press any key to continue...").unwrap();
     stdout.flush().unwrap();
 
